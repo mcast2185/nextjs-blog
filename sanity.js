89da02bc15bc196361b5cc918ece43d5f197b9a2
mcast2185 <76968385+mcast2185@users.noreply.github.com>
2022-06-08 12:10:16 -0600

    first commit

---
+++ sanity.js
@@ -0,0 +1,22 @@
+import {
+  createImageUrlBuilder,
+  createCurrentUserHook,
+  createClient
+  } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const config = {
+
+
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "531i1mga",
+  apiVersion: "2021-03-25",
+
+
+  useCdn: process.env.NODE_ENV === "production",
+};
+
+export const sanityClient = createClient(config); 
+const builder = imageUrlBuilder(config)
+export const urlFor = (source) => builder.image(source);
+export const useCurrentUser = createCurrentUserHook(config);
